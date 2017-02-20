8994268bd81430580b97b51e255506daa7123b83
HyungJun Kim <kim@hyungjunui-macbook-air.local>
2017-02-20 13:19:22 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by HyungJun Kim on 2017. 2. 20..
+//  Copyright © 2017년 HyungJun Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
